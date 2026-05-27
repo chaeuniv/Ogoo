d5f3c48dc1c1c2f10d1ebc2f4e244b4db7d1f0f1
hvvup <hvvijung@gmail.com>
2026-05-27 20:55:54 +0900

    fix/supabase 설정 수정

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
+<<<<<<< HEAD
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabasePublishableKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey);
+=======
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+);
+>>>>>>> 8ba33d7 (fix/supabase 설정 수정)
